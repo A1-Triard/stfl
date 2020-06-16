48eb3857fc50ab5df48315ca00d61faa53f0b7c3
warlock <internalmike@gmail.com>
2020-06-17 02:35:36 +0300

    2

--- base.rs
+++ base.rs
@@ -163,17 +163,7 @@ pub type __off64_t = c_long;
 pub type _IO_lock_t = ();
 pub type __off_t = c_long;
 pub type FILE = _IO_FILE;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct stfl_form {
-    pub root: *mut stfl_widget,
-    pub current_focus_id: c_int,
-    pub cursor_x: c_int,
-    pub cursor_y: c_int,
-    pub event_queue: *mut stfl_event,
-    pub event: *mut wchar_t,
-    pub mtx: pthread_mutex_t,
-}
+
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub union pthread_mutex_t {
@@ -200,58 +190,7 @@ pub struct __pthread_internal_list {
     pub __prev: *mut __pthread_internal_list,
     pub __next: *mut __pthread_internal_list,
 }
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct stfl_event {
-    pub next: *mut stfl_event,
-    pub event: *mut wchar_t,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct stfl_widget {
-    pub parent: *mut stfl_widget,
-    pub next_sibling: *mut stfl_widget,
-    pub first_child: *mut stfl_widget,
-    pub last_child: *mut stfl_widget,
-    pub kv_list: *mut stfl_kv,
-    pub type_0: *mut stfl_widget_type,
-    pub id: c_int,
-    pub x: c_int,
-    pub y: c_int,
-    pub w: c_int,
-    pub h: c_int,
-    pub min_w: c_int,
-    pub min_h: c_int,
-    pub cur_x: c_int,
-    pub cur_y: c_int,
-    pub parser_indent: c_int,
-    pub allow_focus: c_int,
-    pub setfocus: c_int,
-    pub internal_data: *mut c_void,
-    pub name: *mut wchar_t,
-    pub cls: *mut wchar_t,
-}
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct stfl_widget_type {
-    pub name: *mut wchar_t,
-    pub f_init: Option<unsafe extern "C" fn(_: *mut stfl_widget) -> ()>,
-    pub f_done: Option<unsafe extern "C" fn(_: *mut stfl_widget) -> ()>,
-    pub f_enter: Option<unsafe extern "C" fn(_: *mut stfl_widget,
-                                             _: *mut stfl_form) -> ()>,
-    pub f_leave: Option<unsafe extern "C" fn(_: *mut stfl_widget,
-                                             _: *mut stfl_form) -> ()>,
-    pub f_prepare: Option<unsafe extern "C" fn(_: *mut stfl_widget,
-                                               _: *mut stfl_form) -> ()>,
-    pub f_draw: Option<unsafe extern "C" fn(_: *mut stfl_widget,
-                                            _: *mut stfl_form, _: *mut WINDOW)
-                           -> ()>,
-    pub f_process: Option<unsafe extern "C" fn(_: *mut stfl_widget,
-                                               _: *mut stfl_widget,
-                                               _: *mut stfl_form, _: wchar_t,
-                                               _: c_int)
-                              -> c_int>,
-}
+
 pub type WINDOW = _win_st;
 #[derive(Copy, Clone)]
 #[repr(C)]
@@ -305,27 +244,68 @@ pub struct pdat {
     pub _pad_bottom: c_short,
     pub _pad_right: c_short,
 }
-/*
- *  STFL - The Structured Terminal Forms Language/Library
- *  Copyright (C) 2006, 2007  Clifford Wolf <clifford@clifford.at>
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 3 of the License, or (at your option) any later version.
- *  
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *  MA 02110-1301 USA
- *
- *  stfl_internals.h: The STFL C header file (Internal STFL APIs)
- */
+
+#[derive(Copy, Clone)]
+#[repr(C)]
+pub union pthread_mutexattr_t {
+    pub __size: [c_char; 4],
+    pub __align: c_int,
+}
+
+#[repr(C)]
+pub struct stfl_form {
+    pub root: *mut stfl_widget,
+    pub current_focus_id: c_int,
+    pub cursor_x: c_int,
+    pub cursor_y: c_int,
+    pub event_queue: *mut stfl_event,
+    pub event: *mut wchar_t,
+    pub mtx: pthread_mutex_t,
+}
+
+#[repr(C)]
+pub struct stfl_event {
+    pub next: *mut stfl_event,
+    pub event: *mut wchar_t,
+}
+
+#[repr(C)]
+pub struct stfl_widget {
+    pub parent: *mut stfl_widget,
+    pub next_sibling: *mut stfl_widget,
+    pub first_child: *mut stfl_widget,
+    pub last_child: *mut stfl_widget,
+    pub kv_list: *mut stfl_kv,
+    pub type_0: *mut stfl_widget_type,
+    pub id: c_int,
+    pub x: c_int,
+    pub y: c_int,
+    pub w: c_int,
+    pub h: c_int,
+    pub min_w: c_int,
+    pub min_h: c_int,
+    pub cur_x: c_int,
+    pub cur_y: c_int,
+    pub parser_indent: c_int,
+    pub allow_focus: c_int,
+    pub setfocus: c_int,
+    pub internal_data: *mut c_void,
+    pub name: *mut wchar_t,
+    pub cls: *mut wchar_t,
+}
+
+#[repr(C)]
+pub struct stfl_widget_type {
+    pub name: *mut wchar_t,
+    pub f_init: Option<unsafe extern "C" fn(_: *mut stfl_widget) -> ()>,
+    pub f_done: Option<unsafe extern "C" fn(_: *mut stfl_widget) -> ()>,
+    pub f_enter: Option<unsafe extern "C" fn(_: *mut stfl_widget, _: *mut stfl_form) -> ()>,
+    pub f_leave: Option<unsafe extern "C" fn(_: *mut stfl_widget, _: *mut stfl_form) -> ()>,
+    pub f_prepare: Option<unsafe extern "C" fn(_: *mut stfl_widget, _: *mut stfl_form) -> ()>,
+    pub f_draw: Option<unsafe extern "C" fn(_: *mut stfl_widget, _: *mut stfl_form, _: *mut WINDOW) -> ()>,
+    pub f_process: Option<unsafe extern "C" fn(_: *mut stfl_widget, _: *mut stfl_widget, _: *mut stfl_form, _: wchar_t, _: c_int) -> c_int>,
+}
+
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub struct stfl_kv {
@@ -336,33 +316,7 @@ pub struct stfl_kv {
     pub name: *mut wchar_t,
     pub id: c_int,
 }
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub union pthread_mutexattr_t {
-    pub __size: [c_char; 4],
-    pub __align: c_int,
-}
-/*
- *  STFL - The Structured Terminal Forms Language/Library
- *  Copyright (C) 2007  Clifford Wolf <clifford@clifford.at>
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 3 of the License, or (at your option) any later version.
- *  
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *  MA 02110-1301 USA
- *
- *  stfl_compat.h: Some compatibility hacks for b0rken architectures
- */
+
 #[inline]
 unsafe extern "C" fn compat_wcsdup(mut src: *const wchar_t) -> *mut wchar_t {
     let mut n: size_t =
@@ -374,27 +328,7 @@ unsafe extern "C" fn compat_wcsdup(mut src: *const wchar_t) -> *mut wchar_t {
     memcpy(dest as *mut c_void, src as *const c_void, n);
     return dest;
 }
-/*
- *  STFL - The Structured Terminal Forms Language/Library
- *  Copyright (C) 2006, 2007  Clifford Wolf <clifford@clifford.at>
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 3 of the License, or (at your option) any later version.
- *  
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *  MA 02110-1301 USA
- *
- *  base.c: Core functions
- */
+
 #[no_mangle]
 pub static mut stfl_widget_types: [*mut stfl_widget_type; 12] =
     unsafe {
